f12662b6c9090b368a6b57e7ac85aa998db9b09a
KitaniIslam <slmkitani@gmail.com>
2020-03-24 18:11:17 +0100

    set e2e script testing

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,9 +1,25 @@
-import { controller } from '@foal/core';
+import { controller , Hook , Options , HttpResponseNoContent , Context } from '@foal/core';
 
-import { ApiController } from './controllers';
+import { ApiController, AuthController } from './controllers';
+
+@Hook(() => response => {
+  // Every response of this controller and its sub-controllers will be added this header.
+  response.setHeader('Access-Control-Allow-Origin', '*');
+})
 
 export class AppController {
   subControllers = [
     controller('/api', ApiController),
+    controller('/auth', AuthController)
   ];
+
+  @Options('*')
+  options(ctx: Context) {
+    const response = new HttpResponseNoContent();
+    response.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
+    // You may need to allow other headers depending on what you need.
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    return response;
+  }
+
 }
--- src/app/controllers/api.controller.ts
+++ src/app/controllers/api.controller.ts
@@ -1,10 +1,18 @@
-import { Context, Get, HttpResponseOK } from '@foal/core';
+import { Context, Get, HttpResponseOK , TokenRequired , HttpResponseNotFound } from '@foal/core';
+import { TypeORMStore } from '@foal/typeorm';
+import {User} from '../entities/user.entity';
+import { getRepository } from 'typeorm';
 
+@TokenRequired({ store: TypeORMStore })
 export class ApiController {
 
-  @Get('/')
-  index(ctx: Context) {
-    return new HttpResponseOK('Hello world!');
+  @Get('/notes')
+  async index(ctx: Context) {
+    const notes = await getRepository(User).findOne({email: ctx.request.body.email});
+    if(!notes){
+      return new HttpResponseNotFound();
+    }
+    return new HttpResponseOK(notes.email);
   }
 
 }
