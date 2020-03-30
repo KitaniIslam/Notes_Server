8ab7eee7c21346aa7a5c09cc6796f4d58845b60c
KitaniIslam <slmkitani@gmail.com>
2020-03-30 02:43:55 +0100

    add/delete/update notes endpoints

--- src/app/controllers/api.controller.ts
+++ src/app/controllers/api.controller.ts
@@ -1,7 +1,19 @@
-import { Context, Get, HttpResponseOK , TokenRequired , ValidateParams, HttpResponseBadRequest } from '@foal/core';
+import { Context, Get, Post , Patch, HttpResponseOK , TokenRequired , ValidateParams, HttpResponseBadRequest, ValidateBody, Delete, HttpResponseMethodNotAllowed } from '@foal/core';
 import { TypeORMStore } from '@foal/typeorm';
 import {User} from '../entities/user.entity';
-import { getRepository } from 'typeorm';
+import {Category} from '../entities/category.entity';
+import {Note} from '../entities/note.entity'
+import { getRepository} from 'typeorm';
+
+const bodySchema = {
+  additionalProperties: false,
+  properties: {
+    note: {type: 'string'},
+    category : {type: 'string'}
+  },
+  required: [ 'note','category' ],
+  type: 'object',
+};
 
 @TokenRequired({ store: TypeORMStore })
 export class ApiController {
@@ -22,4 +34,57 @@ export class ApiController {
       notes:user.notes
     });
   }
+
+  @Post('/note')
+  @ValidateBody(bodySchema)
+  async note(ctx: Context){
+    const user = await getRepository(User).findOne(ctx.user);
+    const category = await getRepository(Category).findOne({name:ctx.request.body.category});
+    if(!user || !category ){
+      return new HttpResponseBadRequest({message:'Category or User not exists'})
+    }
+    const note = new Note();
+    note.note = ctx.request.body.note;
+    note.category = category;
+    note.user = user;
+    await getRepository(Note).save(note);
+    return new HttpResponseOK({message: 'Your Note added successfully'})
+  }
+
+  @Delete('/note')
+  @ValidateParams({ properties: { id: { type: 'number'} }, type: 'object' })
+  async deleteNote(ctx: Context){
+    const note = await getRepository(Note).findOne({id: ctx.request.query.id,user: ctx.user})
+    if(!note){
+      return new HttpResponseMethodNotAllowed()
+    }
+
+    await getRepository(Note).delete(note);
+    return new HttpResponseOK({message: 'Your Note daleted successfully'});
+
+  }
+
+  @Patch('/note')
+  @ValidateBody({
+    additionalProperties: false,
+    properties: { 
+      id: { type: 'number'},
+      note: {type: 'string'}
+    },
+    type: 'object', 
+    required: [ 'note','id' ]
+  })
+  async updateNote(ctx: Context){
+    const note = await getRepository(Note).findOne({id: ctx.request.body.id,user: ctx.user});
+
+    if(!note){
+      return new HttpResponseMethodNotAllowed();
+    }
+
+    Object.assign(note, {note:ctx.request.body.note});
+    await getRepository(Note).save(note);
+
+    return new HttpResponseOK({message: 'Your Note updated successfully'})
+  }
+
 }
